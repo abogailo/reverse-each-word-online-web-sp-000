8551e5d22572e5f74f7800ca9fbe8340912eaa79
Abagail Hudson <abagailanne@gmail.com>
2019-05-05 14:38:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,4 @@
-def reverse_each_word(sentence)
-  sentence.split.collect do |word|
-     word.reverse
-   end.join(" ")
+def reverse_each_word(words)
+  str = ""
+words.reverse_each { |word| str += "#{word} " }
 end
